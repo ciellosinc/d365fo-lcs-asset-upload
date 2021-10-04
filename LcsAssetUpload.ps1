8535708b17cd92f89463f0988be35e81aa1ef011
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-10-04 17:07:30 +0300

    Create LcsAssetUpload.ps1

---
+++ LcsAssetUpload.ps1
@@ -0,0 +1,17 @@
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [string]$ClientId,
+        [Parameter()]
+        [string]$Username,
+        [string]$Password,
+        $ProjectId,
+        [string]$FilePath,
+        [string]$FileName)
+
+Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
+Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
+Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+Get-D365LcsApiToken -ClientId $ClientId -Username $Username -Password $Password -LcsApiUri "https://lcsapi.lcs.dynamics.com" -Verbose | Set-D365LcsApiConfig -ProjectId $ProjectId
+return Invoke-D365LcsUpload -FilePath $FilePath -FileType "SoftwareDeployablePackage" -FileName $FileName
+
