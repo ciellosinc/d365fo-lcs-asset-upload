ec92707c715e87e79c56bcd7dae149f859b02e4a
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-10-06 00:07:12 +0300

    Update LcsAssetUpload.ps1

--- LcsAssetUpload.ps1
+++ LcsAssetUpload.ps1
@@ -8,7 +8,7 @@
         $ProjectId,
         [string]$FilePath,
         [string]$FileName)
-
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
 Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
 Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
