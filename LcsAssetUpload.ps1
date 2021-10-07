928fbd4dae86d7d2ffaa073f88689358bbea2b78
ciellos-dev <67259746+ciellos-dev@users.noreply.github.com>
2021-10-07 13:19:22 +0300

    Update LcsAssetUpload.ps1

--- LcsAssetUpload.ps1
+++ LcsAssetUpload.ps1
@@ -9,9 +9,19 @@
         [string]$FilePath,
         [string]$FileName)
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
-Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
-Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+#Check Modules installed
+if( -not ((Get-PackageProvider -Name nuget).Name -eq 'NuGet'))
+{
+    Install-PackageProvider nuget -Scope CurrentUser -Force -Confirm:$false
+}
+if( -not ((Get-InstalledModule -Name AZ).Name -eq 'Az'))
+{
+    Install-Module -Name AZ -AllowClobber -Scope CurrentUser -Force -Confirm:$False -SkipPublisherCheck
+}
+if( -not ((Get-InstalledModule -Name d365fo.tools).Name -eq 'd365fo.tools'))
+{
+    Install-Module -Name d365fo.tools -AllowClobber -Scope CurrentUser -Force -Confirm:$false
+}
 Get-D365LcsApiToken -ClientId $ClientId -Username $Username -Password $Password -LcsApiUri "https://lcsapi.lcs.dynamics.com" -Verbose | Set-D365LcsApiConfig -ProjectId $ProjectId
 return Invoke-D365LcsUpload -FilePath $FilePath -FileType "SoftwareDeployablePackage" -FileName $FileName
 
